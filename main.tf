713c09f148b370e5efdc8c1913a46f6d77104f23
kkywalk2 <kkywalk2@gmail.com>
2023-07-23 19:53:16 +0900

    feat: add sftpgo container

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "docker_image" "sftpgo" {
+  name         = "drakkan/sftpgo"
+  keep_locally = false
+}
+
+resource "docker_container" "sftpgo" {
+  image = docker_image.sftpgo.image_id
+  name  = "sftpgo-container"
+  env = ["SFTPGO_WEBDAVD__BINDINGS__0__PORT=10080"]
+
+  ports {
+    internal = 8080
+    external = 8083
+  }
+
+  ports {
+    internal = 10080
+    external = 8084
+  }
+
+  volumes {
+    container_path = "/var/dav"
+    read_only = false
+    host_path = "/var/dav"
+  }
+}
